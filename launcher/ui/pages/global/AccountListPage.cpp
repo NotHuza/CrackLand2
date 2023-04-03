2286f079bed3bcac895b4551b1863e6d6939573c
Magich <jakovmarkovanovic@gmail.com>
2023-04-03 14:20:09 +0200

    cry
    Signed-off-by: Magich <jakovmarkovanovic@gmail.com>

--- launcher/ui/pages/global/AccountListPage.cpp
+++ launcher/ui/pages/global/AccountListPage.cpp
@@ -65,7 +65,7 @@ AccountListPage::AccountListPage(QWidget *parent)
     ui->setupUi(this);
     ui->listView->setEmptyString(tr(
         "Welcome!\n"
-        "If you're new here, you can click the \"Add\" button to add your Mojang or Minecraft account."
+        "If you're new here, you can click the \"Add\" button to add your Mojang , Minecraft account or offline accaunt."
     ));
     ui->listView->setEmptyMode(VersionListView::String);
     ui->listView->setContextMenuPolicy(Qt::CustomContextMenu);
@@ -188,18 +188,18 @@ void AccountListPage::on_actionAddMicrosoft_triggered()
 
 void AccountListPage::on_actionAddOffline_triggered()
 {
-    if (!m_accounts->anyAccountIsValid()) {
-        QMessageBox::warning(
-            this,
-            tr("Error"),
-            tr(
-                "You must add a Microsoft or Mojang account that owns Minecraft before you can add an offline account."
-                "<br><br>"
-                "If you have lost your account you can contact Microsoft for support."
-            )
-        );
-        return;
-    }
+   // if (!m_accounts->anyAccountIsValid()) {
+      //  QMessageBox::warning(
+          //  this,
+          //  tr("Error"),
+          //  tr(
+             //   "You must add a Microsoft or Mojang account that owns Minecraft before you can add an offline account."
+             //   "<br><br>"
+             //   "If you have lost your account you can contact Microsoft for support."
+           // )
+       // );
+       // return;
+   // }
 
     MinecraftAccountPtr account = OfflineLoginDialog::newAccount(
         this,
